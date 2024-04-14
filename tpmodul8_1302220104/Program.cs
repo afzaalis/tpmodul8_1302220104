b03d9cfd545617e89d0d06156ef2667a76f3982b
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2024-04-14 23:40:08 +0700

    menambahkan tugas

--- tpmodul8_1302220104/Program.cs
+++ tpmodul8_1302220104/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tp_tpmodul8_1302220104;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        covidConfig config = covidConfig.LoadConfig();
+
+        config.UbahSatuan();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}");
+        double suhuBadan = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+        double hariDeman = Convert.ToDouble(Console.ReadLine());
+
+        bool kondisi1 = (config.SatuanSuhu == "celcius" && suhuBadan >= 36.5 && suhuBadan <= 37.5) ||
+                        (config.SatuanSuhu == "fahrenheit" && suhuBadan >= 97.7 && suhuBadan <= 99.5);
+        bool kondisi2 = hariDeman < config.BatasHariDeman;
+
+        if (kondisi1 && kondisi2)
+        {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+    }
+}
---
+++ tpmodul8_1302220104/covidConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace tp_tpmodul8_1302220104
+{
+    internal class covidConfig
+    {
+        public string SatuanSuhu { get; set; }
+        public double BatasHariDeman { get; set; }
+        public string PesanDitolak { get; set; }
+        public string PesanDiterima { get; set; }
+
+        internal static covidConfig LoadConfig()
+        {
+            string configFilePath = "covid_config.json";
+
+            // Mengecek apakah file konfigurasi ada
+            if (File.Exists(configFilePath))
+            {
+                // Memuat nilai konfigurasi dari file JSON
+                string json = File.ReadAllText(configFilePath);
+                return JsonConvert.DeserializeObject<covidConfig>(json);
+            }
+            else
+            {
+                return new covidConfig
+                {
+                    SatuanSuhu = "celcius",
+                    BatasHariDeman = 14,
+                    PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                    PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                };
+            }
+        }
+
+        internal void UbahSatuan()
+        {
+            if (SatuanSuhu == "celcius")
+            {
+                SatuanSuhu = "fahrenheit";
+            }
+            else
+            {
+                SatuanSuhu = "celcius";
+            }
+        }
+    }
+}
+
